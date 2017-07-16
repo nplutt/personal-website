5a0541a75675df5afa682a9a3356f733655f1c47
nplutt <nplutt@gmail.com>
2017-07-16 13:59:36 -0500

    Build script that I think works...

--- build.sh
+++ build.sh
@@ -4,15 +4,14 @@ set -e
 
 # Create vitrualenv with node
 pip install virtualenv
-ls
 virtualenv node
-ls
-. /node/bin/activate
+. node/bin/activate
 pip install nodeenv
 nodeenv -p
 
 # Build the UI
 npm install
+npm link @angular/cli
 ng build --target=production --environment=prod
 
 # Deploy the UI to the web S3 bucket
