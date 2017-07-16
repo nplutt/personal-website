3632ae231a52c9c7a368707f182f449304962d82
nplutt <nplutt@gmail.com>
2017-07-16 16:44:28 -0500

    fas

--- build.sh
+++ build.sh
@@ -3,7 +3,6 @@
 set -e
 
 # Create vitrualenv with node
-cd ..
 pip install virtualenv
 virtualenv node
 . node/bin/activate
@@ -11,11 +10,8 @@ pip install nodeenv
 nodeenv -p
 
 # Build the UI
-# npm install
 npm install @angular/cli
 npm link @angular/cli
-ng new mucking
-cd mucking
 npm install
 ng build --target=production --environment=prod
 
