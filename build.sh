431984a4f074c6c583eee3498636b5854b57129b
nplutt <nplutt@gmail.com>
2017-07-16 15:53:51 -0500

    messing aroud

--- build.sh
+++ build.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
+set -e
+
 # Create vitrualenv with node
-#virtualenv node
-#. node/bin/activate
-#pip install nodeenv
-#nodeenv -p
+pip install virtualenv
+virtualenv node
+. node/bin/activate
+pip install nodeenv
+nodeenv -p
 
 # Build the UI
+ls
 npm install
 npm link @angular/cli
+ng new mucking
+cd mucking
+npm install
 ng build --target=production --environment=prod
 
 # Deploy the UI to the web S3 bucket
