e4b6c0c3310008f5044a8aa0b1a737ddc4e45ad1
Nick Plutt <nplutt@gmail.com>
2017-10-23 19:16:46 -0500

    updating ci test runner to run headless chrome with --no-sandbox

--- karma.conf.js
+++ karma.conf.js
@@ -29,7 +29,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome', 'ChromeHeadless'],
-    singleRun: false
+    browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessCI'],
+    singleRun: false,
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    }
   });
 };
