d37cc453f9afd19e7813c22786e24165726789f5
nplutt <nplutt@gmail.com>
2019-01-07 10:10:31 -0600

    fixing sitemap

--- scripts/sitemap.js
+++ scripts/sitemap.js
@@ -2,18 +2,26 @@ const fs = require('fs');
 const fmt = require('xml-formatter');
 const sm = require('sitemap');
 
-const sitemap = sm.createSitemap({
-    hostname: 'https://nickplutt.com',
-    urls: [
-        { url: '/' },
-        { url: '/about' },
-        { url: '/blog' },
-        { url: '/'}
-    ]
-});
+let urls = [
+    { url: '/' },
+    { url: '/about' },
+    { url: '/portfolio' },
+    { url: '/blog'}
+];
 
-sitemap.toXML((err, xml) => {
-   fs.writeFile('./build/sitemap.xml', fmt(xml, {}), () => {
-       console.log('Sitemap created!');
-   });
+fs.readdir('./public/blog-posts', (err, files) => {
+    files.forEach(file => {
+        urls.push({url: `/blog/${file.split('.')[0]}`})
+    });
+
+    const sitemap = sm.createSitemap({
+        hostname: 'https://nickplutt.com',
+        urls: urls
+    });
+
+    sitemap.toXML((err, xml) => {
+        fs.writeFile('./build/sitemap.xml', fmt(xml, {}), () => {
+            console.log('Sitemap created!');
+        });
+    });
 });
--- src/blog/posts.js
+++ src/blog/posts.js
@@ -1,9 +1,10 @@
-import '../utils/common';
-
-export default [
+const posts = [
     {
         title: 'Post 1',
         date: '1/7/2019',
         summary: 'Hello world this is a dummy blog post'
     }
 ]
+
+module.exports = posts;
+export default posts;
